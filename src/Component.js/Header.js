f05bc0d706a22cc24e470ffc4c1f9a341ceb4b41
RishiRagu2510 <rishiragu25@gmail.com>
2023-10-05 11:21:28 +0530

    Blog-App

---
+++ src/Component.js/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+    <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#myblog">My Blog</a></li>
+            <li><a href="#Signin">Sign In</a></li>
+            <li><a href="#signup">Sign Up</a></li>
+        </ul>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/Component.js/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section class="home" id="home">
+        <div class="home-text">
+            <center><h1>Hello, Welcome to My Blog</h1></center>
+            <p>&emsp;&emsp;&emsp;Love, relationships, and dating are the most popular subjects on the internet. We all want to know what is going on in other people’s lives, and we love reading about happy endings. Love is a feeling that just seems to be able to transcend time and space.<br></br> <br></br> 
+            &emsp;&emsp;&emsp;which is why it’s no surprise that many bloggers choose this topic for their blog posts. Blogging has become an important part of building connections with our readers because not only do they want us to write about things that interest them, but also share our own stories with them as well!</p>
+        </div>
+    </section>
+  )
+}
+
+export default Home
